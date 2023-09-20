355ec4e27f4da11421b5c91dc6733e2d3183bba1
Carlos <aguilac@seas.upenn.edu>
2023-09-20 17:20:39 -0400

    fixed the spike detector pre-processing

--- HUMAN/spike_detector/iEEG_helper_functions.py
+++ HUMAN/spike_detector/iEEG_helper_functions.py
@@ -1,14 +1,11 @@
 # This file should not use pandas
 
 import numpy as np
-from scipy.signal import butter, filtfilt, iirnotch
+from scipy.signal import butter, filtfilt, iirnotch, iirfilter
 
 
-def notch_filter(data, low_cut, high_cut, fs, order=4):
-    nyq = 0.5 * fs
-    low = low_cut / nyq
-    high = high_cut / nyq
-    b, a = iirnotch(w0=(low + high) / 2, Q=30, fs=fs)
+def notch_filter(data, hz, fs):
+    b, a = iirnotch(hz, Q=30, fs=fs)
     y = filtfilt(b, a, data, axis=0)
     return y
 
@@ -102,6 +99,8 @@ def electrode_selection(labels):
             if "O1" in set(
                 labels
             ):  # if hemiscalp, should not have odd; if ieeg, should have O1
+                select[i] = 1
+            else:
                 select[i] = 0
     return select
 
@@ -251,4 +250,44 @@ def butter_bp_filter(data, lowcut, highcut, fs, order=3):
     """
     bandpass_b, bandpass_a = butter(order, [lowcut, highcut], btype='bandpass', fs=fs)
     signal_bp = filtfilt(bandpass_b, bandpass_a, data, axis=0)
+    return signal_bp
+
+def new_bandpass_filt(data, lowcut, highcut, fs, order = 4):
+
+    if fs == 1024:
+        b = [0.0342089439540094, 0, -0.0684178879080189, 0, 0.0342089439540094]
+        a = [1, -3.40837438183729, 4.36773330454651, -2.50912852732152, 0.549774904492375]
+        signal_bp = filtfilt(b, a, data, axis=0)
+
+    elif fs == 512:
+        b = [0.110341876092030, 0, -0.220683752184059, 0, 0.110341876092030]
+        a = [1, -2.84999684509203, 3.01525532511459, -1.47261949410732, 0.307429302286222]
+        signal_bp = filtfilt(b, a, data, axis=0)
+
+    elif fs == 256:
+        b = [0.329773746685091, 0, -0.659547493370182, 0, 0.329773746685091]
+        a = [0.175233821173555, -0.200810082151730, 0.830452461803929, -1.80406441093276, 1]
+        signal_bp = filtfilt(b, a, data, axis=0)
+
+    elif fs == 500:
+        b = [0.114657122916782, 0, -0.229314245833564, 0, 0.114657122916782]
+        a = [1, -2.82405915548544, 2.95589050957282, -1.43119381095927, 0.299436856070785]
+        signal_bp = filtfilt(b, a, data, axis=0)
+
+    elif fs == 1000:
+        b = [0.0356639677619672, 0, -0.0713279355239343, 0, 0.0356639677619672]
+        a = [1, -3.39453926569963, 4.33233599727022, -2.47975692956295, 0.541965991567219]
+        signal_bp = filtfilt(b, a, data, axis=0)
+
+    elif fs == 250:
+        b = [0.342416923871813, 0, -0.684833847743626, 0, 0.342416923871813]
+        a = [1, -1.75428158626788, 0.736712673740108, -0.159617332038591, 0.178069768015428]
+        signal_bp = filtfilt(b, a, data, axis=0)
+
+    else:
+        print("Sampling Frequency is not covered by this function, this is the best approximation")
+        print("Spike rates/counts will not be affected by more than 5 percent of the original value")
+        bandpass_b, bandpass_a = butter(order, [lowcut, highcut], btype='bandpass', fs=fs)
+        signal_bp = filtfilt(bandpass_b, bandpass_a, data, axis=0)
+
     return signal_bp
--- HUMAN/spike_detector/validation.py
+++ HUMAN/spike_detector/validation.py
@@ -1,12 +1,80 @@
+#%% import
 #import libraries
 import os
 import numpy as np
 import pandas as pd
 from ieeg.auth import Session
+import matplotlib.pyplot as plt
 
 # Import custom functions
 from get_iEEG_data import *
 from spike_detector import *
 from iEEG_helper_functions import *
 
+#%% get data
+# Get the data
+password_bin_filepath = "/mnt/leif/littlab/users/aguilac/tools/agu_ieeglogin.bin"
+with open(password_bin_filepath, "r") as f:
+    session = Session("aguilac", f.read())
 
+blocktime = [279792,279852] #in seconds #checks out 1 to 1
+blocktime = [92919,92979] #1 to 1
+blocktime = [181085,181145] # missing 4 spikes, leaders are right, duplicates in the same channels are dropped
+
+dataset_name = "HUP210_phaseII"
+
+dataset = session.open_dataset(dataset_name)
+all_channel_labels = np.array(dataset.get_channel_labels())
+channel_labels_to_download = all_channel_labels[
+    electrode_selection(all_channel_labels)
+]
+
+
+ieeg_data, fs = get_iEEG_data(
+    "aguilac",
+    password_bin_filepath,
+    dataset_name,
+    blocktime[0]*1e6,
+    blocktime[1]*1e6,
+    channel_labels_to_download,
+)
+
+fs = int(fs)
+
+#%%spike detector
+# clean the data
+good_channels_res = detect_bad_channels_optimized(ieeg_data.to_numpy(), fs)
+good_channel_indicies = good_channels_res[0]
+good_channel_labels = channel_labels_to_download[good_channel_indicies]
+ieeg_data = ieeg_data[good_channel_labels].to_numpy()
+
+ieeg_data = common_average_montage(ieeg_data)
+
+# Apply the filters directly on the DataFrame
+ieeg_data = notch_filter(ieeg_data, 60, fs) #less than 1 microvolt difference
+ieeg_data = new_bandpass_filt(ieeg_data, 1, 70, fs, order = 4) #few differences here by a couple of microvolts
+
+# Detect spikes
+spike_output = spike_detector(
+    data=ieeg_data,
+    fs=fs,
+    electrode_labels=good_channel_labels,
+)
+
+#clean output
+spike_output = spike_output.astype(int)
+actual_number_of_spikes = len(spike_output)
+
+channel_labels_mapped = good_channel_labels[spike_output[:, 1]]
+
+spike_output_df = pd.DataFrame(
+    spike_output, columns=["peak_index", "channel_label", "sequence_index"]
+)
+spike_output_df["channel_label"] = channel_labels_mapped
+
+#%%
+spike_output_df.groupby('sequence_index').count()
+spike_output_df[spike_output_df['sequence_index'] == 0]
+
+
+#%%
