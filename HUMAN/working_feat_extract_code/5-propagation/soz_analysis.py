7c9a6528d8e102fd25371b7b137789d45a199187
Carlos <aguilac@seas.upenn.edu>
2023-11-10 16:22:20 -0500

    all_elecs concat, plots

--- HUMAN/working_feat_extract_code/5-propagation/soz_analysis.py
+++ HUMAN/working_feat_extract_code/5-propagation/soz_analysis.py
@@ -49,15 +49,75 @@ print(mesial_temp_spikes['pt_id'].nunique())
 print(non_mesial_temp_spikes['pt_id'].nunique())
 
 # %%
-#find the average of each electrode for each patient
-mesial_temp_spikes_avg = mesial_temp_spikes.groupby(['pt_id', 'channel_label']).mean()
-non_mesial_temp_spikes_avg = non_mesial_temp_spikes.groupby(['pt_id', 'channel_label']).mean()
+#strip the letters from the channel_label column and keep only the numerical portion
+mesial_temp_spikes['channel_label'] = mesial_temp_spikes['channel_label'].str.replace('L|R|A|H|B|C|D', '')
+non_mesial_temp_spikes['channel_label'] = non_mesial_temp_spikes['channel_label'].str.replace('L|R|A|H|B|C|D', '')
+
+#replace "sharpness" with the absolute value of it
+mesial_temp_spikes['sharpness'] = abs(mesial_temp_spikes['sharpness'])
+non_mesial_temp_spikes['sharpness'] = abs(non_mesial_temp_spikes['sharpness'])
+
+#group by patient and channel_label and get the average spike rate for each patient and channel
+mesial_temp_spikes_avg = mesial_temp_spikes.groupby(['pt_id', 'channel_label'])['sharpness'].mean().reset_index()
+non_mesial_temp_spikes_avg = non_mesial_temp_spikes.groupby(['pt_id', 'channel_label'])['sharpness'].mean().reset_index()
+
+# for mesial_temp_spikes_avg, add a column called 'mesial' and set it to 1
+mesial_temp_spikes_avg['is_mesial'] = 1
+# for non_mesial_temp_spikes_avg, add a column called 'mesial' and set it to 0
+non_mesial_temp_spikes_avg['is_mesial'] = 0
+
+#concatenate mesial_temp_spikes_avg and non_mesial_temp_spikes_avg
+all_spikes_avg = pd.concat([mesial_temp_spikes_avg, non_mesial_temp_spikes_avg], axis=0).reset_index(drop=True)
+all_spikes_avg = all_spikes_avg.pivot_table(index=['pt_id','is_mesial'], columns='channel_label', values='sharpness')
+all_spikes_avg = all_spikes_avg.reindex(columns=['1','2','3','4','5','6','7','8','9','10','11','12'])
+
+#reorder all_spikes_avg, so that is_mesial is decesending
+all_spikes_avg = all_spikes_avg.sort_values(by=['is_mesial', 'pt_id'], ascending=[False, True])
 
 # %%
 #create a heat map where each row is a patient from pt_id and each column is a channel from channel_label
 #the values are the average spike rate for each patient and channel
-mesial_temp_spikes_avg = mesial_temp_spikes_avg.pivot_table(index='pt_id', columns='channel_label', values='avg_latency')
-non_mesial_temp_spikes_avg = non_mesial_temp_spikes_avg.pivot_table(index='pt_id', columns='channel_label', values='avg_latency')
+mesial_temp_spikes_avg = mesial_temp_spikes_avg.pivot_table(index='pt_id', columns='channel_label', values='sharpness')
+non_mesial_temp_spikes_avg = non_mesial_temp_spikes_avg.pivot_table(index='pt_id', columns='channel_label', values='sharpness')
+
+#reorder columns so goes in [1,2,3,4,5,6,7,8,9,10,11,12]
+mesial_temp_spikes_avg = mesial_temp_spikes_avg.reindex(columns=['1','2','3','4','5','6','7','8','9','10','11','12'])
+non_mesial_temp_spikes_avg = non_mesial_temp_spikes_avg.reindex(columns=['1','2','3','4','5','6','7','8','9','10','11','12'])
+
+#%%
+#plot mesial_temp_spikes_avg, non_mesial_temp_spikes_avg in a heatmap 
+import seaborn as sns
+import matplotlib.pyplot as plt
+plt.figure(figsize=(10,10))
+sns.heatmap(mesial_temp_spikes_avg, cmap='viridis')
+plt.show()
+plt.figure(figsize=(10,10))
+sns.heatmap(non_mesial_temp_spikes_avg, cmap='viridis')
+plt.show()
+
+#%%
+#color in all the mesial temporal channels
+plt.figure(figsize=(15,15))
+sns.heatmap(all_spikes_avg, cmap='viridis', alpha = 1)
+plt.xlabel('Channel Number', fontsize=16)
+plt.ylabel('Patient ID', fontsize=16)
+plt.title('Average Sharpness by Channel and Patient', fontsize=20)
+plt.axhline(25, color='r', linewidth=2)
+#change y-tick labels to only be the first element in the index, making the first 25 red and the rest black
+plt.yticks(np.arange(0.5, len(all_spikes_avg.index), 1), all_spikes_avg.index.get_level_values(0))
+#create a list of 48 colors
+colors = ['r']*25 + ['k']*23
+for ytick, color in zip(plt.gca().get_yticklabels(), colors):
+    ytick.set_color(color)
+
+#add a legend that has red == mesial temporal patients and black == non-mesial temporal patients
+import matplotlib.patches as mpatches
+red_patch = mpatches.Patch(color='red', label='Mesial Temporal')
+black_patch = mpatches.Patch(color='black', label='Non-Mesial Temporal')
+plt.legend(handles=[red_patch, black_patch], loc='upper right')
+
+# plt.savefig('figures/allpts_bySOZ_mesial.png', dpi = 300)
+plt.show()
 
 
 # %%
