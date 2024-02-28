414d1095798129b6d6fee71d08bdfb7aa331b06b
Carlos <aguilac@seas.upenn.edu>
2024-02-28 14:57:01 -0500

    Moran's I

--- HUMAN/working_feat_extract_code/5-propagation/AP_soz_analysis.py
+++ HUMAN/working_feat_extract_code/5-propagation/AP_soz_analysis.py
@@ -27,7 +27,7 @@ data_directory = ['/mnt/leif/littlab/users/aguilac/Projects/FC_toolbox/results/m
 # %%  ADD BILATERAL PATIENTS
 # KEEP THE SAME SIDE, PLUS FOR BILATERAL TAKE BOTH SIDES
 
-Feat_of_interest = 'linelen' #recruiment_latency
+Feat_of_interest = 'rise_amp' #recruiment_latency
 take_spike_leads = False
 remove_other_pts = True
 
@@ -123,18 +123,24 @@ mesial_temp_spikes_avg['SOZ'] = 1
 all_spikes_avg = pd.concat([mesial_temp_spikes_avg, non_mesial_temp_spikes_avg], axis=0).reset_index(drop=True)
 all_spikes_avg[['char', 'num']] = all_spikes_avg['channel_label'].str.extract(r'([ABCD])(\d+)')
 
-def relabel(group):
-    # Calculate the starting number of the next group
-    start_num = (group['num'].max() // 3 + 1) * 3
-    # Generate the new labels
-    new_labels = group['char'] + start_num.astype(str)
-    return new_labels
+# Convert 'num' column to numeric type
+all_spikes_avg['num'] = pd.to_numeric(all_spikes_avg['num'], errors='coerce')
 
-# Apply the function to each group
-all_spikes_avg['new_label'] = all_spikes_avg.groupby('char').apply(relabel).reset_index(drop=True)
+# Drop rows with NaN values in 'num' column
+all_spikes_avg = all_spikes_avg.dropna(subset=['num'])
+
+def map_to_nearest_multiple_of_3(num):
+    return int(3 * ((num - 1) // 3 + 1))
+
+# Apply the mapping function to the 'num' column
+all_spikes_avg['num'] = all_spikes_avg['num'].apply(map_to_nearest_multiple_of_3)
+all_spikes_avg['relabel'] = all_spikes_avg['char'] + all_spikes_avg['num'].astype(str)
+
 
 #%%
-all_spikes_avg_test = all_spikes_avg.pivot_table(index=['pt_id','SOZ'], columns='char', values=Feat_of_interest)
+all_spikes_avg_test = all_spikes_avg.pivot_table(index=['pt_id','SOZ'], columns='relabel', values=Feat_of_interest)
+# all_spikes_avg_test = all_spikes_avg_test[['A3','B3','C3','A6','B6','C6','A9','B9','C9','A12','B12','C12']]
+all_spikes_avg_test = all_spikes_avg_test[['A3','A6','A9','A12','B3','B6','B9','B12','C3','C6','C9','C12']]
 
 #%%
 
@@ -143,7 +149,7 @@ all_spikes_avg_test = all_spikes_avg.pivot_table(index=['pt_id','SOZ'], columns=
 # all_spikes_avg = all_spikes_avg.reindex(columns=['1','2','3','4','5','6','7','8','9','10','11','12'])
 
 # #reorder all_spikes_avg, so that is_mesial is decesending
-# all_spikes_avg = all_spikes_avg.sort_values(by=['SOZ', 'pt_id'], ascending=[True, True])
+all_spikes_avg_test = all_spikes_avg_test.sort_values(by=['SOZ', 'pt_id'], ascending=[True, True])
 
 
 # #create a heat map where each row is a patient from pt_id and each column is a channel from channel_label
@@ -213,5 +219,106 @@ plt.legend(handles=[mesial_patch, other_patch, temporal_patch, neocort_patch], l
 
 # plt.savefig(f'figures/sameside_perSOZ/bilateral/{Feat_of_interest}_allptsbySOZ.png', dpi = 300)
 plt.show()
+
+# %%
+###################
+###################
+###################
+# MORAN's I
+###################
+###################
+###################
+###################
+
+#SOME MORAN'S I work
+from libpysal.weights import lat2W
+from esda.moran import Moran
+import numpy as np
+
+moran_matrices = all_spikes_avg.dropna()
+
+who = []
+moranI = []
+
+for i, rows in moran_matrices.iterrows():
+    # Use your matrix here, instead of this random one
+    Z  = np.array(rows).reshape(3, 4)
+    # Create the matrix of weigthts 
+    w = lat2W(Z.shape[0], Z.shape[1])
+    # Create the pysal Moran object 
+    mi = Moran(Z, w)
+    # Verify Moran's I results 
+    who.append(i)
+    moranI.append(mi.I)
+
+#lets get boxplots for Moran's I
+    
+moran_df = pd.DataFrame(who, columns = ['pt_id','SOZ'])
+moran_df['I'] = moranI
+
+plt.figure(figsize=(10,10))
+my_palette = {1:'#E64B35FF', 'other cortex':'#7E6148FF', 'temporal':'#3C5488FF', 'temporal neocortical':'#00A087FF'}
+#change font to arial
+plt.rcParams['font.family'] = 'Arial'
+sns.violinplot(x='SOZ', y='I', data=moran_df, palette=my_palette, inner = 'point')
+# sns.stripplot(x = 'SOZ', y = 'I', data = moran_df, color = 'k', jitter = 0.2, size =5)
+plt.xlabel('SOZ Type', fontsize=12)
+plt.ylabel("Moran's I", fontsize=12)
+plt.title("Moran's I for a subset of patients with full electrode data", fontsize = 16)
+#change the x-tick labels to be more readable
+plt.xticks(np.arange(4), ['Mesial Temporal', 'Other Cortex', 'Temporal', 'Neocortical'], fontsize = 12)
+plt.yticks(fontsize = 12)
+plt.show()
+
+#HEATMAP for Moran's I
+
+all_spikes_avg = moran_matrices
+#plot mesial_temp_spikes_avg, non_mesial_temp_spikes_avg in a heatmap 
+import seaborn as sns
+import matplotlib.pyplot as plt
+plt.clf()
+#color in all the mesial temporal channels
+plt.figure(figsize=(20,20))
+
+sns.heatmap(all_spikes_avg, cmap='viridis', alpha = 1)#, vmin=0, vmax=125)
+
+plt.xlabel('Channel Number', fontsize=20)
+plt.ylabel('Patient ID', fontsize=20)
+plt.title(f'Average {Feat_of_interest} by Channel and Patient', fontsize=24)
+#change y-tick labels to only be the first element in the index, making the first 25 red and the rest black
+plt.yticks(np.arange(0.5, len(all_spikes_avg.index), 1), all_spikes_avg.index.get_level_values(0), fontsize=13)
+
+
+#in all_spikes_avg, get the number of 'temporal neocortical' patients
+temp_neocort_pts = len(all_spikes_avg[all_spikes_avg.index.get_level_values(1) == 'temporal neocortical'])
+#in all_spikes_avg, get the number of 'temporal' patients
+temp_pts = len(all_spikes_avg[all_spikes_avg.index.get_level_values(1) == 'temporal'])
+#same for other cortex
+other_cortex_pts = len(all_spikes_avg[all_spikes_avg.index.get_level_values(1) == 'other cortex'])
+#same for mesial temporal
+mesial_temp_pts = len(all_spikes_avg[all_spikes_avg.index.get_level_values(1) == 1])
+
+#seperate the patients with a line to show where it splits
+plt.axhline(mesial_temp_pts, color='k', linewidth=2.5)
+plt.axhline(mesial_temp_pts+other_cortex_pts, color='k', linewidth=1.5, linestyle = '--')
+plt.axhline(mesial_temp_pts+other_cortex_pts+temp_pts, color='k', linewidth=1.5, linestyle = '--')
+
+#create a list of 48 colors
+colors = ['#E64B35FF']*mesial_temp_pts + ['#7E6148FF']*other_cortex_pts + ['#00A087FF']*temp_pts + ['#3C5488FF']*temp_neocort_pts
+for ytick, color in zip(plt.gca().get_yticklabels(), colors):
+    ytick.set_color(color)
+
+#add a legend that has red == mesial temporal patients and black == non-mesial temporal patients
+import matplotlib.patches as mpatches
+mesial_patch = mpatches.Patch(color='#E64B35FF', label='Mesial Temporal Patients')
+other_patch = mpatches.Patch(color='#7E6148FF', label='Other Cortex Patients')
+temporal_patch = mpatches.Patch(color='#00A087FF', label='Temporal Patients')
+neocort_patch = mpatches.Patch(color='#3C5488FF', label='Temporal Neocortical Patients')
+
+plt.legend(handles=[mesial_patch, other_patch, temporal_patch, neocort_patch], loc='upper right')
+
+# plt.savefig(f'figures/sameside_perSOZ/bilateral/{Feat_of_interest}_allptsbySOZ.png', dpi = 300)
+plt.show()
+
 
 # %%
