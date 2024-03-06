7ec3097ce1a4455a6c499d580cabf2ccb9efc6d1
Carlos <aguilac@seas.upenn.edu>
2024-03-06 16:55:55 -0500

    fixed spatial temporal analysis for MUSC

--- HUMAN/working_feat_extract_code/5-propagation/MUSC/soz_analysis.py
+++ HUMAN/working_feat_extract_code/5-propagation/MUSC/soz_analysis.py
@@ -23,33 +23,29 @@ sys.path.append(code_path)
 from ied_fx_v3 import *
 
 data_directory = ['/mnt/leif/littlab/users/aguilac/Projects/FC_toolbox/results/mat_output_v2', '/mnt/leif/littlab/data/Human_Data']
-#%%
-MUSC_spikes = pd.read_csv('../dataset/MUSC_allspikes.csv')
-# %%
-# ADD BILATERAL PATIENTS
+
+## load the spike data
+MUSC_spikes = pd.read_csv('../dataset/MUSC_allspikes_v2.csv', index_col=0)
+
+#load SOZ corrections
+MUSC_sozs = pd.read_excel('/mnt/leif/littlab/users/aguilac/Projects/FC_toolbox/results/mat_output_v2/pt_data/MUSC-soz-corrections.xlsx')
+MUSC_sozs = MUSC_sozs[MUSC_sozs['Site_1MUSC_2Emory'] == 1]
+
+#fix SOZ and laterality
+MUSC_spikes = MUSC_spikes.merge(MUSC_sozs, left_on = 'pt_id', right_on = 'ParticipantID', how = 'inner')
+MUSC_spikes = MUSC_spikes.drop(columns=['ParticipantID','Site_1MUSC_2Emory','IfNeocortical_Location','Correction Notes','lateralization_left','lateralization_right','region'])
+
+# ADD MUSC PATIENTS
 # KEEP THE SAME SIDE, PLUS FOR BILATERAL TAKE BOTH SIDES
 
-Feat_of_interest = 'rise_amp'
+Feat_of_interest = 'spike_rate'
 take_spike_leads = False
 
-####################
-# 1. Load in data  #
-####################
+#########################
+# 1. Organize the data  #
+#########################
 
-#load spikes from dataset
-if ('rate' in Feat_of_interest) | ('latency' in Feat_of_interest) | (Feat_of_interest == 'seq_spike_time_diff'):
-    all_spikes = pd.read_csv('dataset/spikes_bySOZ_T-R.csv', index_col=0)
-    bilateral_spikes = pd.read_csv('dataset/bilateral_spikes_bySOZ_T-R.csv', index_col=0)
-else:
-    all_spikes = pd.read_csv('dataset/spikes_bySOZ.csv')
-    bilateral_spikes = pd.read_csv('dataset/bilateral_MTLE_all_spikes.csv')
-    bilateral_spikes = bilateral_spikes.drop(['engel','hup_id','name','spike_rate'], axis=1)
-
-
-#rename 'clinic_SOZ' to 'SOZ'
-bilateral_spikes = bilateral_spikes.rename(columns={'clinic_SOZ':'SOZ'})
-
-all_spikes = pd.concat([all_spikes, bilateral_spikes], axis=0).reset_index(drop=True)
+all_spikes = MUSC_spikes
 
 #flag that says we want spike leaders only
 if take_spike_leads == True:
@@ -59,40 +55,54 @@ if take_spike_leads == True:
 # all_spikes = all_spikes[~all_spikes['SOZ'].str.contains('other')].reset_index(drop=True)
 
 #channels to keep 
-chs_tokeep = ['RA','LA','RDA','LDA','LH','RH','LDH','RDH','DA','DH','DHA','LB','LDB','LC','LDC','RB','RDB','RC','RDC']
+chs_tokeep = ['RA','LA','LPH','RPH','LAH','RAH']
 
 #if channel_label contains any of the strings in chs_tokeep, keep it
 all_spikes = all_spikes[all_spikes['channel_label'].str.contains('|'.join(chs_tokeep))].reset_index(drop=True)
 
+#remove any channels that contains letters that shouldn't be there
+all_spikes = all_spikes[~all_spikes['channel_label'].str.contains('I|LAP|T|S|C')].reset_index(drop=True)
+
+## fixes to only have same-side spikes
 #only take the electrode channels that are in the same side
-left_spikes = all_spikes[all_spikes['SOZ'].str.contains('left')].reset_index(drop=True)
+left_spikes = all_spikes[((all_spikes['Left'] == 1) & (all_spikes['Right'] == 0))].reset_index(drop=True)
 left_spikes_tokeep = left_spikes[~left_spikes['channel_label'].str.contains('R')].reset_index(drop=True)
 
-right_spikes = all_spikes[all_spikes['SOZ'].str.contains('right')].reset_index(drop=True)
+right_spikes = all_spikes[((all_spikes['Left'] == 0) & (all_spikes['Right'] == 1))].reset_index(drop=True)
 right_spikes_tokeep = right_spikes[~right_spikes['channel_label'].str.contains('L')].reset_index(drop=True)
 
-bilateral_spikes = all_spikes[all_spikes['SOZ'].str.contains('bilateral')].reset_index(drop=True)
+bilateral_spikes = all_spikes[((all_spikes['Left'] == 1) & (all_spikes['Right'] == 1))].reset_index(drop=True)
 
 #concat them back into all_spikes
 all_spikes = pd.concat([left_spikes_tokeep, right_spikes_tokeep, bilateral_spikes], axis =0).reset_index(drop=True)
 
+def soz_assigner(row):
+    if row['MTL'] == 1:
+        return 1
+    elif row['Neo'] == 1:
+        return 2
+    elif row['Temporal'] == 1:
+        return 4
+    elif row['Other'] == 1:
+        return 3
+    else:
+        return None
+
+all_spikes['region'] = all_spikes.apply(soz_assigner, axis = 1)
+
 #get only the spikes that contain 'mesial temporal' in the SOZ column
-mesial_temp_spikes = all_spikes[all_spikes['SOZ'].str.contains('mesial')].reset_index(drop=True)
+mesial_temp_spikes = all_spikes[all_spikes['region'] == 1].reset_index(drop=True)
 
 # grab the remaining spikes that aren't in mesial_temp_spikes
-non_mesial_temp_spikes = all_spikes[~all_spikes['SOZ'].str.contains('mesial')].reset_index(drop=True)
-
-#remove any 'channel_label' that contains the letter T or F
-mesial_temp_spikes = mesial_temp_spikes[~mesial_temp_spikes['channel_label'].str.contains('T|F|P|RCC|RCA|RAD|LAD|LHD|RHD|LDAH|RDAH|RCB|Z')].reset_index(drop=True)
-non_mesial_temp_spikes = non_mesial_temp_spikes[~non_mesial_temp_spikes['channel_label'].str.contains('T|F|P|RCC|RCA|RAD|LAD|LHD|RHD|LDAH|RDAH|RCB|Z')].reset_index(drop=True)
+non_mesial_temp_spikes = all_spikes[~(all_spikes['region'] == 1)].reset_index(drop=True)
 
 ########################################
 # 2. Filter Elecs, Group, and Analysis #
 ########################################
 
 #strip the letters from the channel_label column and keep only the numerical portion
-mesial_temp_spikes['channel_label'] = mesial_temp_spikes['channel_label'].str.replace('L|R|A|H|B|C|D', '')
-non_mesial_temp_spikes['channel_label'] = non_mesial_temp_spikes['channel_label'].str.replace('L|R|A|H|B|C|D', '')
+mesial_temp_spikes['channel_label'] = mesial_temp_spikes['channel_label'].str.replace('L|R|A|H|P', '')
+non_mesial_temp_spikes['channel_label'] = non_mesial_temp_spikes['channel_label'].str.replace('L|R|A|H|P', '')
 
 #replace "sharpness" with the absolute value of it
 mesial_temp_spikes[Feat_of_interest] = abs(mesial_temp_spikes[Feat_of_interest])
@@ -100,30 +110,19 @@ non_mesial_temp_spikes[Feat_of_interest] = abs(non_mesial_temp_spikes[Feat_of_in
 
 #group by patient and channel_label and get the average spike rate for each patient and channel
 mesial_temp_spikes_avg = mesial_temp_spikes.groupby(['pt_id', 'channel_label'])[Feat_of_interest].mean().reset_index()
-#for non_mesial_temp_spikes_avg['SOZ'], only keep everything after '_'
-non_mesial_temp_spikes['SOZ'] = non_mesial_temp_spikes['SOZ'].str.split('_').str[1]
-non_mesial_temp_spikes_avg = non_mesial_temp_spikes.groupby(['pt_id', 'channel_label', 'SOZ'])[Feat_of_interest].mean().reset_index()
+mesial_temp_spikes_avg['region'] = 1
 
-# for mesial_temp_spikes_avg, add a column called 'mesial' and set it to 1
-mesial_temp_spikes_avg['SOZ'] = 1
+#for non_mesial_temp_spikes_avg['SOZ'], only keep everything after '_'
+non_mesial_temp_spikes_avg = non_mesial_temp_spikes.groupby(['pt_id', 'channel_label', 'region'])[Feat_of_interest].mean().reset_index()
+
 
 #concatenate mesial_temp_spikes_avg and non_mesial_temp_spikes_avg
 all_spikes_avg = pd.concat([mesial_temp_spikes_avg, non_mesial_temp_spikes_avg], axis=0).reset_index(drop=True)
-all_spikes_avg = all_spikes_avg.pivot_table(index=['pt_id','SOZ'], columns='channel_label', values=Feat_of_interest)
-all_spikes_avg = all_spikes_avg.reindex(columns=['1','2','3','4','5','6','7','8','9','10','11','12'])
+all_spikes_avg = all_spikes_avg.pivot_table(index=['pt_id','region'], columns='channel_label', values=Feat_of_interest)
+all_spikes_avg = all_spikes_avg.reindex(columns=['1','2','3','4','5','6','7','8','9','10'])
 
 #reorder all_spikes_avg, so that is_mesial is decesending
-all_spikes_avg = all_spikes_avg.sort_values(by=['SOZ', 'pt_id'], ascending=[True, True])
-
-
-#create a heat map where each row is a patient from pt_id and each column is a channel from channel_label
-#the values are the average spike rate for each patient and channel
-mesial_temp_spikes_avg = mesial_temp_spikes_avg.pivot_table(index='pt_id', columns='channel_label', values=Feat_of_interest)
-non_mesial_temp_spikes_avg = non_mesial_temp_spikes_avg.pivot_table(index='pt_id', columns='channel_label', values=Feat_of_interest)
-
-#reorder columns so goes in [1,2,3,4,5,6,7,8,9,10,11,12]
-mesial_temp_spikes_avg = mesial_temp_spikes_avg.reindex(columns=['1','2','3','4','5','6','7','8','9','10','11','12'])
-non_mesial_temp_spikes_avg = non_mesial_temp_spikes_avg.reindex(columns=['1','2','3','4','5','6','7','8','9','10','11','12'])
+all_spikes_avg = all_spikes_avg.sort_values(by=['region', 'pt_id'], ascending=[True, True])
 
 #remove 'HUP215' from all_spikes_avg
 if ('latency' in Feat_of_interest) | (Feat_of_interest == 'seq_spike_time_diff'):
@@ -137,26 +136,12 @@ if ('latency' in Feat_of_interest) | (Feat_of_interest == 'seq_spike_time_diff')
 #plot mesial_temp_spikes_avg, non_mesial_temp_spikes_avg in a heatmap 
 import seaborn as sns
 import matplotlib.pyplot as plt
-# plt.figure(figsize=(10,10))
-# sns.heatmap(mesial_temp_spikes_avg, cmap='viridis')
-# plt.show()
-# plt.figure(figsize=(10,10))
-# sns.heatmap(non_mesial_temp_spikes_avg, cmap='viridis')
-# plt.show()
+
 plt.clf()
 #color in all the mesial temporal channels
 plt.figure(figsize=(20,20))
 
-# if Feat_of_interest == 'spike_rate':
-#     sns.heatmap(all_spikes_avg, cmap='viridis', alpha = 1, vmin=0, vmax=15)
-# if Feat_of_interest == 'sharpness':
-#     sns.heatmap(all_spikes_avg, cmap='viridis', alpha = 1, vmin=0, vmax=200)
-# if Feat_of_interest == 'linelen':
-#     sns.heatmap(all_spikes_avg, cmap='viridis', alpha = 1, vmin=0, vmax=4300)
-# if Feat_of_interest == 'slow_max':
-#     sns.heatmap(all_spikes_avg, cmap='viridis', alpha = 1, vmin=0, vmax=900)
-# else:
-sns.heatmap(all_spikes_avg, cmap='viridis', alpha = 1, vmin=0, vmax=200)
+sns.heatmap(all_spikes_avg, cmap='viridis', alpha = 1)
 
 plt.xlabel('Channel Number', fontsize=20)
 plt.ylabel('Patient ID', fontsize=20)
@@ -165,30 +150,28 @@ plt.title(f'Average {Feat_of_interest} by Channel and Patient', fontsize=24)
 plt.yticks(np.arange(0.5, len(all_spikes_avg.index), 1), all_spikes_avg.index.get_level_values(0), fontsize=13)
 
 #in all_spikes_avg, get the number of 'temporal neocortical' patients
-temp_neocort_pts = len(all_spikes_avg[all_spikes_avg.index.get_level_values(1) == 'temporal neocortical'])
+temp_neocort_pts = len(all_spikes_avg[all_spikes_avg.index.get_level_values(1) == 2])
 #in all_spikes_avg, get the number of 'temporal' patients
-temp_pts = len(all_spikes_avg[all_spikes_avg.index.get_level_values(1) == 'temporal'])
+temp_pts = len(all_spikes_avg[all_spikes_avg.index.get_level_values(1) == 4])
 #same for other cortex
-other_cortex_pts = len(all_spikes_avg[all_spikes_avg.index.get_level_values(1) == 'other cortex'])
+other_cortex_pts = len(all_spikes_avg[all_spikes_avg.index.get_level_values(1) == 3])
 #same for mesial temporal
 mesial_temp_pts = len(all_spikes_avg[all_spikes_avg.index.get_level_values(1) == 1])
 
 plt.axhline(mesial_temp_pts, color='k', linewidth=2.5)
-plt.axhline(mesial_temp_pts+other_cortex_pts, color='k', linewidth=1.5, linestyle = '--')
-plt.axhline(mesial_temp_pts+other_cortex_pts+temp_pts, color='k', linewidth=1.5, linestyle = '--')
-#create a list of 48 colors
-colors = ['#E64B35FF']*mesial_temp_pts + ['#7E6148FF']*other_cortex_pts + ['#00A087FF']*temp_pts + ['#3C5488FF']*temp_neocort_pts
+plt.axhline(mesial_temp_pts+temp_neocort_pts, color='k', linewidth=1.5, linestyle = '--')
+#create a list of 31 colors
+colors = ['#E64B35FF']*mesial_temp_pts + ['#3C5488FF']*temp_neocort_pts + ['#7E6148FF']*other_cortex_pts
 for ytick, color in zip(plt.gca().get_yticklabels(), colors):
     ytick.set_color(color)
 
 #add a legend that has red == mesial temporal patients and black == non-mesial temporal patients
 import matplotlib.patches as mpatches
 mesial_patch = mpatches.Patch(color='#E64B35FF', label='Mesial Temporal Patients')
-other_patch = mpatches.Patch(color='#7E6148FF', label='Other Cortex Patients')
-temporal_patch = mpatches.Patch(color='#00A087FF', label='Temporal Patients')
 neocort_patch = mpatches.Patch(color='#3C5488FF', label='Temporal Neocortical Patients')
+other_patch = mpatches.Patch(color='#7E6148FF', label='Other Cortex Patients')
 
-plt.legend(handles=[mesial_patch, other_patch, temporal_patch, neocort_patch], loc='upper right')
+plt.legend(handles=[mesial_patch, neocort_patch, other_patch], loc='upper right')
 
 # plt.savefig(f'figures/sameside_perSOZ/bilateral/{Feat_of_interest}_allptsbySOZ.png', dpi = 300)
 plt.show()
