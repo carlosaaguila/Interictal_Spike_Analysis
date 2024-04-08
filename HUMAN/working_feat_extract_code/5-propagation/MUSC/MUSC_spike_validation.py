b1bbf4ca908f7dc60107b22035e0d7d4a154b36b
Carlos <aguilac@seas.upenn.edu>
2024-04-08 10:02:53 -0400

    new log file where images get saved

--- HUMAN/working_feat_extract_code/5-propagation/MUSC/MUSC_spike_validation.py
+++ HUMAN/working_feat_extract_code/5-propagation/MUSC/MUSC_spike_validation.py
@@ -46,7 +46,7 @@ MUSC_spikes = MUSC_spikes[~MUSC_spikes['pt_id'].isin(pts_to_remove)]
 spikes = MUSC_spikes
 to_plot = MUSC_spikes['filename'].unique()[0:10] #change if you want more filenames to look @.
 
-with open("/mnt/leif/littlab/users/aguilac/Interictal_Spike_Analysis/HUMAN/working_feat_extract_code/5-propagation/error_log.txt", "a") as log_file:
+with open("/mnt/leif/littlab/users/aguilac/Projects/FC_toolbox/results/mat_output_v2/supplemental_figs/MUSC_rand_spikes/MUSC_rand_spike_LOG", "a") as log_file:
     for index, row in MUSC_spikes.iterrows():
         pt_id = row['pt_id']
         filename = row['filename']
