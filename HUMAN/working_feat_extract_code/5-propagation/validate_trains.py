30e243ec359d9d183a431330a693183cb1b7e40d
Carlos <aguilac@seas.upenn.edu>
2024-05-23 08:31:19 -0400

    code to plot trains.

--- HUMAN/working_feat_extract_code/5-propagation/validate_trains.py
+++ HUMAN/working_feat_extract_code/5-propagation/validate_trains.py
@@ -69,16 +69,18 @@ def plot_train(train, eeg_data):
             RIGHT = PEAK + SS['new_right']
             LEFT = PEAK + SS['new_left']
             print("this is the x-value of the peak:",PEAK)
-            axs[i].plot(range(lower_bound_idx, upper_bound_idx), eeg_data[ch_to_plot].iloc[lower_bound_idx:upper_bound_idx])
+            axs[i].plot(range(lower_bound_idx, upper_bound_idx), eeg_data[ch_to_plot].iloc[lower_bound_idx:upper_bound_idx], color = 'k', lw = 3)
             axs[i].get_xaxis().set_visible(False)
-            axs[i].plot(PEAK, eeg_data[ch_to_plot].iloc[PEAK], 'x', color = 'r')
-            axs[i].plot(LEFT, eeg_data[ch_to_plot].iloc[LEFT], 'x', color = 'r')
-            axs[i].plot(RIGHT, eeg_data[ch_to_plot].iloc[RIGHT], 'x', color = 'r')
+            axs[i].plot(PEAK, eeg_data[ch_to_plot].iloc[PEAK], 'o', color = '#00a087', markersize = 7 )
 
+            # axs[i].plot(LEFT, eeg_data[ch_to_plot].iloc[LEFT], 'x', color = 'r')
+            # axs[i].plot(RIGHT, eeg_data[ch_to_plot].iloc[RIGHT], 'x', color = 'r')
         else:
             print(f'{ch_to_plot} is a BAD CHANNEL')
         plt.subplots_adjust(hspace=0.1)
-
+        sns.despine()
+        plt.tight_layout()
+        plt.savefig('figures/MUSC+HUP/official/example_train.pdf')
 #%%
 #get a patient row.
 row = pt_ids.iloc[2]
@@ -89,19 +91,17 @@ filename = row['filename']
 spikes_oi = spikes[spikes['pt_id'] == pt_id]
 
 #grab a random spike train index
-spike_train_index = spikes_oi['new_spike_seq'].sample(1).values
+# spike_train_index = spikes_oi['new_spike_seq'].sample(1).values
+spike_train_index = np.array([5102])
 
 #grab the random spike train
 train = spikes_oi[spikes_oi['new_spike_seq'] == spike_train_index[0]][['peak_index', 'new_spike_seq','peak_time_usec','channel_label','channel_index','peak','left_point','right_point','slow_end']]
 
-#%%
 train['new_peak'] = (train['peak'] - 1000)
 train['new_left'] = train['left_point'] - 1000
 train['new_right'] = train['right_point'] - 1000
 train['order_of_sorting'] = train['peak_index'] + train['new_peak']
 
-
-# %%
 #Load in the spike.
 with open("/mnt/leif/littlab/users/aguilac/tools/agu_ieeglogin.bin", "r") as f:
     session = Session("aguilac", f.read())
@@ -144,21 +144,6 @@ CAR_data = common_average_montage(ieeg_data)
 CAR_data_filt = new_bandpass_filt(CAR_data, 1, 70, fs, order=4)
 CAR_data = pd.DataFrame(CAR_data_filt, columns = good_channel_labels)
 
-# %%
 plot_train(train, CAR_data)
-# # %%
-# #make it 2 seconds before spike train starts
-# lower_bound_idx = 30*fs - int(0.25*fs)
-# #make it 2 seconds AFTER spike trains last onset
-# upper_bound_idx = np.shape(CAR_data)[0] - (30*fs) + int(0.25*fs)
 
-# #line them up 
-# train['order_of_sorting'] = train['peak_index'] + train['new_peak']
-# train = train.sort_values(by = 'order_of_sorting', ascending = True).reset_index(drop=True)
-
-# first_spike = train['order_of_sorting'].min()
-
-# SS = train.iloc[0]
-# ch_to_plot = SS['channel_label']
-# plt.plot(range(lower_bound_idx,upper_bound_idx), CAR_data[ch_to_plot].iloc[lower_bound_idx:upper_bound_idx])
-
+# %%
