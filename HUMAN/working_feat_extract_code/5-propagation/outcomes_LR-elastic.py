cdc455ba45cf7575964b72f711344b671a75c62f
Carlos <aguilac@seas.upenn.edu>
2024-09-14 15:43:53 -0400

    changed what a good outcome was

--- HUMAN/working_feat_extract_code/5-propagation/outcomes_LR-elastic.py
+++ HUMAN/working_feat_extract_code/5-propagation/outcomes_LR-elastic.py
@@ -18,7 +18,7 @@ pec_outcomes = pd.read_excel('/mnt/leif/littlab/users/aguilac/Projects/FC_toolbo
 
 # %%
 
-Engel_good = ['IA','IB','IC','ID']
+Engel_good = ['IA','IB']
 
 def calculate_outcome(row):
     if pd.isna(row['engel_f1']):
