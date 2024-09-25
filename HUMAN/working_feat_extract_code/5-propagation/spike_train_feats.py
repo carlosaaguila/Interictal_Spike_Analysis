1653dc8725589a014469e1b5372e29663b236131
Carlos <aguilac@seas.upenn.edu>
2024-09-25 14:45:17 -0400

    spike figs

--- HUMAN/working_feat_extract_code/5-propagation/spike_train_feats.py
+++ HUMAN/working_feat_extract_code/5-propagation/spike_train_feats.py
@@ -25,119 +25,117 @@ from iEEG_helper_functions import *
 
 data_directory = ['/mnt/leif/littlab/users/aguilac/Projects/FC_toolbox/results/mat_output_v2', '/mnt/leif/littlab/data/Human_Data']
 
-#load spikes
-all_spikes = pd.read_csv('/mnt/leif/littlab/users/aguilac/Interictal_Spike_Analysis/HUMAN/working_feat_extract_code/5-propagation/dataset/spikes_bySOZ.csv')
-bilateral_spikes = pd.read_csv('/mnt/leif/littlab/users/aguilac/Interictal_Spike_Analysis/HUMAN/working_feat_extract_code/5-propagation/dataset/bilateral_MTLE_all_spikes.csv')
-bilateral_spikes = bilateral_spikes.drop(['engel','hup_id','name','spike_rate'], axis=1)
+spikes_thresh = pd.read_csv('/mnt/leif/littlab/users/aguilac/Interictal_Spike_Analysis/HUMAN/working_feat_extract_code/5-propagation/dataset/complete_dfs/hup_thresholded.csv', index_col= 0)
+chs_tokeep = ['RA','LA','RDA','LDA','LH','RH','LDH','RDH','DA','DH','DHA','LB','LDB','LC','LDC','RB','RDB','RC','RDC']
 
-#rename 'clinic_SOZ' to 'SOZ'
-bilateral_spikes = bilateral_spikes.rename(columns={'clinic_SOZ':'SOZ'})
-spikes = pd.concat([all_spikes, bilateral_spikes], axis=0).reset_index(drop=True)
+    #if channel_label contains any of the strings in chs_tokeep, keep it
+spikes_thresh = spikes_thresh[spikes_thresh['channel_label'].str.contains('|'.join(chs_tokeep))].reset_index(drop=True)
 
-#load filenames_w_ids.csv 
-filenamescsv = pd.read_csv('/mnt/leif/littlab/users/aguilac/Projects/FC_toolbox/results/mat_output_v2/pt_data/filenames_w_ids.csv')
-filenamescsv = filenamescsv[filenamescsv['to use'] == 1]
-multi_file = filenamescsv[filenamescsv['hup_id'].duplicated()]['hup_id'].unique()
-pt_ids = filenamescsv[~filenamescsv['hup_id'].isin(multi_file)]
+# spikes_thresh = spikes_thresh[spikes_thresh['filename'].str.contains('170|179|185|192|187')]
+spikes_thresh = spikes_thresh[spikes_thresh['filename'].str.contains('192|204|215|105|221')]
+pt_ids = spikes_thresh['filename'].unique()
+spikes = spikes_thresh
 
-#%%
-pt_ids
 with open("/mnt/leif/littlab/users/aguilac/Interictal_Spike_Analysis/HUMAN/working_feat_extract_code/5-propagation/error_log.txt", "a") as log_file:
-    for index, row in pt_ids.iterrows():
-        pt_id = row['hup_id']
-        filename = row['filename']
-
-        rows = 1
-        cols = 1
+    for index, filename in enumerate(pt_ids):
+        pt_id = spikes[spikes['filename'] == filename]['pt_id'].iloc[0]
+        rows = 3
+        cols = 2
         num_plots = rows * cols
-        fig, axes = plt.subplots(rows, cols, figsize=(15,20)) 
+        fig, axes = plt.subplots(rows, cols, figsize=(10,10)) 
 
-        try:
-            for i in range(num_plots):
-                #find all spikes in HUP105
-                spikes_oi = spikes[spikes['pt_id'] == pt_id]
+    
+        for i in range(num_plots):
+            #find all spikes in HUP105
+            spikes_oi = spikes[spikes['filename'] == filename]
 
-                #pick a random spike in record
-                single_spike = spikes_oi.sample(1)
+            #pick a random spike in record
+            single_spike = spikes_oi.sample(1)
 
-                # find important characteristics of that random spike
-                peak_time_usec = single_spike['peak_time_usec']
-                ch_oi = single_spike['channel_label']
+            # find important characteristics of that random spike
+            peak_time_usec = single_spike['peak_time_usec']
+            ch_oi = single_spike['channel_label']
 
-                #Load in the spike.
-                with open("/mnt/leif/littlab/users/aguilac/tools/agu_ieeglogin.bin", "r") as f:
-                    session = Session("aguilac", f.read())
+            #Load in the spike.
+            with open("/mnt/leif/littlab/users/aguilac/tools/agu_ieeglogin.bin", "r") as f:
+                session = Session("aguilac", f.read())
 
-                dataset = session.open_dataset(filename)
+            dataset = session.open_dataset(filename)
 
-                all_channel_labels = np.array(dataset.get_channel_labels())
+            all_channel_labels = np.array(dataset.get_channel_labels())
 
-                #change the sequence_index == X for a different peak_index
-                ch_labels = all_channel_labels[electrode_selection(all_channel_labels)]
+            #change the sequence_index == X for a different peak_index
+            ch_labels = all_channel_labels[electrode_selection(all_channel_labels)]
 
-                #load in 1 minute centered around the spike (for CAR)
-                ieeg_data, fs = get_iEEG_data(
-                            "aguilac",
-                            "/mnt/leif/littlab/users/aguilac/tools/agu_ieeglogin.bin",
-                            filename,
-                            (peak_time_usec) - (30 * 1e6),
-                            (peak_time_usec) + (30 * 1e6),
-                            ch_labels
-                        )
+            #load in 1 minute centered around the spike (for CAR)
+            ieeg_data, fs = get_iEEG_data(
+                        "aguilac",
+                        "/mnt/leif/littlab/users/aguilac/tools/agu_ieeglogin.bin",
+                        filename,
+                        (peak_time_usec) - (30 * 1e6),
+                        (peak_time_usec) + (30 * 1e6),
+                        ch_labels
+                    )
 
-                fs = int(fs)
+            fs = int(fs)
 
-                #look for bad channels
-                good_channels_res = detect_bad_channels_optimized(ieeg_data.to_numpy(), fs)
-                good_channel_indicies = good_channels_res[0]
-                good_channel_labels = ch_labels[good_channel_indicies]
-                ieeg_data = ieeg_data[good_channel_labels].to_numpy()
+            #look for bad channels
+            good_channels_res = detect_bad_channels_optimized(ieeg_data.to_numpy(), fs)
+            good_channel_indicies = good_channels_res[0]
+            good_channel_labels = ch_labels[good_channel_indicies]
+            ieeg_data = ieeg_data[good_channel_labels].to_numpy()
 
-                # Apply CAR Montage
-                CAR_data = common_average_montage(ieeg_data)
+            # Apply CAR Montage
+            CAR_data = common_average_montage(ieeg_data)
 
-                #apply bandpass filter
-                CAR_data_filt = new_bandpass_filt(CAR_data, 1, 70, fs, order=4)
-                CAR_data = pd.DataFrame(CAR_data_filt, columns = good_channel_labels)
+            #apply bandpass filter
+            CAR_data_filt = new_bandpass_filt(CAR_data, 1, 70, fs, order=4)
 
-                #get times surrounding the middle
-                start = int(len(CAR_data[ch_oi])/2 - 1.5*fs)
-                stop = int(len(CAR_data[ch_oi])/2 + 1.5*fs)
-                mid = int(len(CAR_data[ch_oi])/2)
-                plt_samps = np.arange(start,stop,1)
+            good_channel_labels = [decompose_labels(x, pt_id) for x in good_channel_labels]
+            CAR_data = pd.DataFrame(CAR_data_filt, columns = good_channel_labels)
 
-                try:
-                    #plot morphology points
-                    peak = mid + single_spike['peak'].values[0]
-                    left = mid + (single_spike['left_point'].values[0] - 1000)
-                    right = mid + (single_spike['right_point'].values[0] - 1000)
-                    slow_end = mid + (single_spike['slow_end'].values[0]-1000)
-                except:
-                    print('no morphology feats, skip...')
-                    continue
+            
+            #get times surrounding the middle
+            start = int(len(CAR_data[ch_oi])/2 - 1.5*fs)
+            stop = int(len(CAR_data[ch_oi])/2 + 1.5*fs)
+            mid = int(len(CAR_data[ch_oi])/2)
+            plt_samps = np.arange(start,stop,1)
 
-                #plot the spike we are looking for.
-                row = i // cols
-                col = i % cols
-                ax = axes[row,col]
-                ax.plot(plt_samps, CAR_data[ch_oi][start:stop])
-                #plot morphology
-                ax.plot(peak, CAR_data[ch_oi].iloc[peak], 'o', color = 'r', label = 'Peak')
-                ax.plot(left, CAR_data[ch_oi].iloc[left], 'o', color = 'b')
-                ax.plot(right, CAR_data[ch_oi].iloc[right], 'o', color = 'b')
-                ax.plot(slow_end, CAR_data[ch_oi].iloc[slow_end], 'o', color = 'k')
-                ax.set_title(f'Spike at {ch_oi.values[0]} and {int(peak_time_usec.values[0] * 1e-6)} secs')
-        except:
-            print('rerun for:', pt_id)
-            log_file.write(f"An error has occured for {pt_id}\n")
-            continue
+            try:
+                #plot morphology points
+                peak = mid + single_spike['peak'].values[0]
+                left = mid + (single_spike['left_point'].values[0] - 1000)
+                right = mid + (single_spike['right_point'].values[0] - 1000)
+                slow_end = mid + (single_spike['slow_end'].values[0]-1000)
+            except:
+                print('no morphology feats, skip...')
+                continue
 
-        fig.suptitle(f'Random Spikes for {pt_id}', fontsize = 16)
-        plt.subplots_adjust(hspace=0.5)  # Increase  spacing
-        plt.subplots_adjust(top = 0.95)
+            #plot the spike we are looking for.
+            row = i // cols
+            col = i % cols
+            ax = axes[row,col]
+            ax.plot(plt_samps, CAR_data[ch_oi][start:stop])
+            #plot morphology
+            ax.plot(peak, CAR_data[ch_oi].iloc[peak], 'o', color = 'r', label = 'Peak')
+            ax.plot(left, CAR_data[ch_oi].iloc[left], 'o', color = 'b')
+            ax.plot(right, CAR_data[ch_oi].iloc[right], 'o', color = 'b')
+            ax.plot(slow_end, CAR_data[ch_oi].iloc[slow_end], 'o', color = 'k')
+            ax.set_title(f'Spike at {ch_oi.values[0]}')
+            ax.set_xlabel('Sample', fontsize=12)
+            ax.set_ylabel('Amplitude', fontsize=12)
+            ax.tick_params(axis='both', which='major', labelsize=10)
+
+
+        fig.suptitle(f'Random Spikes for {pt_id}', fontsize = 20)
+        plt.tight_layout()
+        # plt.subplots_adjust(hspace=0.5)  # Increase  spacing
+        plt.subplots_adjust(top = 0.93)
         # plt.show()
 
-        # plt.savefig(f'/mnt/leif/littlab/users/aguilac/Projects/FC_toolbox/results/mat_output_v2/supplemental_figs/random_detects_and_morph/{pt_id}_spikes.png', dpi = 600)
+        plt.savefig(f'/mnt/leif/littlab/users/aguilac/Projects/FC_toolbox/results/mat_output_v2/supplemental_figs/random_detects_and_morph/{pt_id}_supplementspikes_2.pdf')
+
+
 
 
 # %%
--- HUMAN/working_feat_extract_code/5-propagation/validate_trains.py
+++ HUMAN/working_feat_extract_code/5-propagation/validate_trains.py
@@ -26,13 +26,8 @@ from iEEG_helper_functions import *
 data_directory = ['/mnt/leif/littlab/users/aguilac/Projects/FC_toolbox/results/mat_output_v2', '/mnt/leif/littlab/data/Human_Data']
 
 #load spikes
-all_spikes = pd.read_csv('/mnt/leif/littlab/users/aguilac/Interictal_Spike_Analysis/HUMAN/working_feat_extract_code/5-propagation/dataset/spikes_bySOZ.csv')
-bilateral_spikes = pd.read_csv('/mnt/leif/littlab/users/aguilac/Interictal_Spike_Analysis/HUMAN/working_feat_extract_code/5-propagation/dataset/bilateral_MTLE_all_spikes.csv')
-bilateral_spikes = bilateral_spikes.drop(['engel','hup_id','name','spike_rate'], axis=1)
-
-#rename 'clinic_SOZ' to 'SOZ'
-bilateral_spikes = bilateral_spikes.rename(columns={'clinic_SOZ':'SOZ'})
-spikes = pd.concat([all_spikes, bilateral_spikes], axis=0).reset_index(drop=True)
+spikes_thresh = pd.read_csv('/mnt/leif/littlab/users/aguilac/Interictal_Spike_Analysis/HUMAN/working_feat_extract_code/5-propagation/dataset/complete_dfs/hup_thresholded.csv', index_col= 0)
+spikes = spikes_thresh
 
 #load filenames_w_ids.csv 
 filenamescsv = pd.read_csv('/mnt/leif/littlab/users/aguilac/Projects/FC_toolbox/results/mat_output_v2/pt_data/filenames_w_ids.csv')
@@ -41,109 +36,119 @@ multi_file = filenamescsv[filenamescsv['hup_id'].duplicated()]['hup_id'].unique(
 pt_ids = filenamescsv[~filenamescsv['hup_id'].isin(multi_file)]
 
 #%%
-def plot_train(train, eeg_data):
-    """
-    This function will take a train dataframe that contains all the information about a spike train & eeg_data, which is a cleaned common avergae refence dataframe.
-    It will return a plot of the spike train, in order of peaks.
-    """
-    #make it 2 seconds before spike train starts
+def plot_train(train, eeg_data, pt_id, i, fs):
     lower_bound_idx = 30*fs - int(0.25*fs)
-    #make it 2 seconds AFTER spike trains last onset
-    upper_bound_idx = np.shape(CAR_data)[0] - (30*fs) + int(0.25*fs)
+    upper_bound_idx = np.shape(eeg_data)[0] - (30*fs) + int(0.25*fs)
     
-    #shape of the subplots we might want to plot on
     rows = train.shape[0]
     cols = 1
-    fig, axs = plt.subplots(rows, cols, figsize=(8, 15), sharex=True)
+    fig, axs = plt.subplots(rows, cols, figsize=(5, 10), sharex=True)
 
-    #line them up 
-    train = train.sort_values(by = 'order_of_sorting', ascending = True).reset_index(drop=True)
-    
+    train = train.sort_values(by='order_of_sorting', ascending=True).reset_index(drop=True)
     first_spike = train['order_of_sorting'].min()
 
-    for i, SS in train.iterrows():
+    for index, SS in train.iterrows():
         ch_to_plot = SS['channel_label']
         if ch_to_plot in eeg_data.columns.to_list():
             diff_from_first = SS['order_of_sorting'] - first_spike
             PEAK = (30*fs) + diff_from_first
             RIGHT = PEAK + SS['new_right']
             LEFT = PEAK + SS['new_left']
-            print("this is the x-value of the peak:",PEAK)
-            axs[i].plot(range(lower_bound_idx, upper_bound_idx), eeg_data[ch_to_plot].iloc[lower_bound_idx:upper_bound_idx], color = 'k', lw = 3)
-            axs[i].get_xaxis().set_visible(False)
-            axs[i].plot(PEAK, eeg_data[ch_to_plot].iloc[PEAK], 'o', color = '#00a087', markersize = 7 )
-
-            # axs[i].plot(LEFT, eeg_data[ch_to_plot].iloc[LEFT], 'x', color = 'r')
-            # axs[i].plot(RIGHT, eeg_data[ch_to_plot].iloc[RIGHT], 'x', color = 'r')
+            print("this is the x-value of the peak:", PEAK)
+            
+            if rows > 1:
+                ax = axs[index]
+            else:
+                ax = axs  # If there's only one subplot, axs is not a list
+            
+            ax.plot(range(lower_bound_idx, upper_bound_idx), eeg_data[ch_to_plot].iloc[lower_bound_idx:upper_bound_idx], color='k', lw=3)
+            ax.get_xaxis().set_visible(False)
+            ax.plot(PEAK, eeg_data[ch_to_plot].iloc[PEAK], 'o', color='#00a087', markersize=7)
         else:
             print(f'{ch_to_plot} is a BAD CHANNEL')
-        plt.subplots_adjust(hspace=0.1)
-        sns.despine()
-        plt.tight_layout()
-        plt.savefig('figures/MUSC+HUP/official/example_train.pdf')
+
+    plt.subplots_adjust(hspace=0.1)
+    sns.despine()
+    plt.tight_layout()
+
+    plt.savefig(f'figures/MUSC+HUP/official/{pt_id}_{i}_example_train.pdf')
+    plt.close(fig)  # Close the figure to free up memory
+
+
 #%%
-#get a patient row.
-row = pt_ids.iloc[2]
-pt_id = row['hup_id']
-filename = row['filename']
+spikes = spikes[spikes['pt_id'].str.contains('170|179|185|192|187')]
+pt_ids_of_interest = spikes['pt_id'].unique()
+pt_ids = pt_ids[pt_ids['hup_id'].isin(pt_ids_of_interest)]
 
-#look for spikes in a specific patient
-spikes_oi = spikes[spikes['pt_id'] == pt_id]
+for index, row in pt_ids.iterrows():
+    #get a patient row.
+    pt_id = row['hup_id']
+    filename = row['filename']
 
-#grab a random spike train index
-# spike_train_index = spikes_oi['new_spike_seq'].sample(1).values
-spike_train_index = np.array([5102])
+    #look for spikes in a specific patient
+    spikes_oi = spikes[spikes['pt_id'] == pt_id]
 
-#grab the random spike train
-train = spikes_oi[spikes_oi['new_spike_seq'] == spike_train_index[0]][['peak_index', 'new_spike_seq','peak_time_usec','channel_label','channel_index','peak','left_point','right_point','slow_end']]
+    for i in range(5):
+        
+        #grab a random spike train index
+        spike_train_index = spikes_oi['new_spike_seq'].sample(1).values
+        # spike_train_index = np.array([5102])
+        #grab the random spike train
+        train = spikes_oi[spikes_oi['new_spike_seq'] == spike_train_index[0]][['peak_index', 'new_spike_seq','peak_time_usec','channel_label','channel_index','peak','left_point','right_point','slow_end']]
+        
+        if (len(train)<3) | (len(train)>10):
+            while (len(train)<3) | (len(train)>10):
+                #grab a random spike train index
+                spike_train_index = spikes_oi['new_spike_seq'].sample(1).values
+                # spike_train_index = np.array([5102])
+                #grab the random spike train
+                train = spikes_oi[spikes_oi['new_spike_seq'] == spike_train_index[0]][['peak_index', 'new_spike_seq','peak_time_usec','channel_label','channel_index','peak','left_point','right_point','slow_end']]
 
-train['new_peak'] = (train['peak'] - 1000)
-train['new_left'] = train['left_point'] - 1000
-train['new_right'] = train['right_point'] - 1000
-train['order_of_sorting'] = train['peak_index'] + train['new_peak']
+        train['new_peak'] = (train['peak'])
+        train['new_left'] = train['left_point'] - 1000
+        train['new_right'] = train['right_point'] - 1000
+        train['order_of_sorting'] = train['peak_index'] + train['new_peak']
 
-#Load in the spike.
-with open("/mnt/leif/littlab/users/aguilac/tools/agu_ieeglogin.bin", "r") as f:
-    session = Session("aguilac", f.read())
+        #Load in the spike.
+        with open("/mnt/leif/littlab/users/aguilac/tools/agu_ieeglogin.bin", "r") as f:
+            session = Session("aguilac", f.read())
 
-dataset = session.open_dataset(filename)
+        dataset = session.open_dataset(filename)
 
-all_channel_labels = np.array(dataset.get_channel_labels())
+        all_channel_labels = np.array(dataset.get_channel_labels())
 
-#change the sequence_index == X for a different peak_index
-ch_labels = all_channel_labels[electrode_selection(all_channel_labels)]
+        #change the sequence_index == X for a different peak_index
+        ch_labels = all_channel_labels[electrode_selection(all_channel_labels)]
 
-fs = int(dataset.get_time_series_details(dataset.ch_labels[0]).sample_rate)  # get sample rate
+        fs = int(dataset.get_time_series_details(dataset.ch_labels[0]).sample_rate)  # get sample rate
 
-#find a minute of data around the spike train we want.
-step_in_usec = (1 * fs) * 1e6 #1second in u_sec
-lower_bound = (train['peak_time_usec']+((train['new_peak']/fs) * 1e6)).min()
-upper_bound = (train['peak_time_usec']+((train['new_peak']/fs) * 1e6)).max()
+        #find a minute of data around the spike train we want.
+        step_in_usec = (1 * fs) * 1e6 #1second in u_sec
+        lower_bound = (train['peak_time_usec']+((train['new_peak']/fs) * 1e6)).min()
+        upper_bound = (train['peak_time_usec']+((train['new_peak']/fs) * 1e6)).max()
 
-ieeg_data, fs = get_iEEG_data(
-                            "aguilac",
-                            "/mnt/leif/littlab/users/aguilac/tools/agu_ieeglogin.bin",
-                            filename,
-                            (lower_bound) - (30 * 1e6),
-                            (upper_bound) + (30 * 1e6),
-                            ch_labels
-                        )
+        ieeg_data, fs = get_iEEG_data(
+                                    "aguilac",
+                                    "/mnt/leif/littlab/users/aguilac/tools/agu_ieeglogin.bin",
+                                    filename,
+                                    (lower_bound) - (30 * 1e6),
+                                    (upper_bound) + (30 * 1e6),
+                                    ch_labels
+                                )
 
-fs = int(fs)
+        fs = int(fs)
 
-#look for bad channels
-good_channels_res = detect_bad_channels_optimized(ieeg_data.to_numpy(), fs)
-good_channel_indicies = good_channels_res[0]
-good_channel_labels = ch_labels[good_channel_indicies]
-ieeg_data = ieeg_data[good_channel_labels].to_numpy()
+        #look for bad channels
+        good_channels_res = detect_bad_channels_optimized(ieeg_data.to_numpy(), fs)
+        good_channel_indicies = good_channels_res[0]
+        good_channel_labels = ch_labels[good_channel_indicies]
+        ieeg_data = ieeg_data[good_channel_labels].to_numpy()
 
-# Apply CAR Montage
-CAR_data = common_average_montage(ieeg_data)
+        # Apply CAR Montage
+        CAR_data = common_average_montage(ieeg_data)
 
-#apply bandpass filter
-CAR_data_filt = new_bandpass_filt(CAR_data, 1, 70, fs, order=4)
-CAR_data = pd.DataFrame(CAR_data_filt, columns = good_channel_labels)
-
-plot_train(train, CAR_data)
-
-# %%
+        #apply bandpass filter
+        CAR_data_filt = new_bandpass_filt(CAR_data, 1, 70, fs, order=4)
+        good_channel_labels = [decompose_labels(x, pt_id) for x in good_channel_labels]
+        CAR_data = pd.DataFrame(CAR_data_filt, columns = good_channel_labels)
+        plot_train(train, CAR_data, pt_id, i, fs)
